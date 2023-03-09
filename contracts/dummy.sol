7d06bcd0c7d4c71d51456aca651427180cde1388
Thanusha Chowdary <101420469+Thanu317@users.noreply.github.com>
2023-03-09 22:55:33 +0530

    update the contarct address

--- contracts/dummy.sol
+++ contracts/dummy.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MITj
 pragma solidity >=0.4.22 <0.9.0;
-
-contract HelloWorld {
-    function sayHelloWorld() public pure returns (string memory) {
-        return "Hello World";
+contract Owner{
+    address  owner;
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
     }
+}
+contract MyContract is Owner {
+    mapping(address => uint) balances;
+    function mint(uint value) onlyOwner public{
+        balances[msg.sender] += value;
+    }    
 }
