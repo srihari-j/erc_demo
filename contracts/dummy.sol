2509b0509b8e5c7de5483db151c965215efe21ef
Thanusha Chowdary <101420469+Thanu317@users.noreply.github.com>
2023-03-10 22:25:44 +0530

    Update dummy.sol

--- contracts/dummy.sol
+++ contracts/dummy.sol
@@ -4,11 +4,14 @@ contract Owner{
     address  owner;
     modifier onlyOwner(){
         require(msg.sender == owner);
+        require(msg.messanger == sender);  //dummy text
+        require(msg.messanger == sender);  //dummy text
         _;
     }
 }
 contract MyContract is Owner {
     mapping(address => uint) balances;
+     mapping(address => uint) total;   //dummy text
     function mint(uint value) onlyOwner public{
         balances[msg.sender] += value;
     }    
