2473406142bfb8710f0cf1336fe6e98fde04c19a
Thanusha Chowdary <101420469+Thanu317@users.noreply.github.com>
2023-03-10 23:20:15 +0530

    Update dummy.sol

--- contracts/dummy.sol
+++ contracts/dummy.sol
@@ -2,11 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 contract Owner{
     address  owner;
+    test 5
     modifier onlyOwner(){
         require(msg.sender == owner);
         require(msg.messanger == sender);  //dummy text
         require(msg.messanger == sender);  //dummy text
         _;
+        test 11
     }
 }
 contract MyContract is Owner {
@@ -16,6 +18,7 @@ contract MyContract is Owner {
     function mint(uint value) onlyOwner public{
         balances[msg.sender] += value;
         hello i am the test 
+        test 21
          mapping(address => uint) total;   //dummy text dummy text
     }    
 }
