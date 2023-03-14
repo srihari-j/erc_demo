a5cd2e31c1091cda577942b3a6dc60d99ba7003e
Thanusha Chowdary <101420469+Thanu317@users.noreply.github.com>
2023-03-14 20:49:29 +0530

    Update dummy.sol

--- contracts/dummy.sol
+++ contracts/dummy.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.4.22 <0.9.0;
 contract Owner{
     address  owner;
-    test 5   dummy test added for refrence
     modifier onlyOwner(){
         require(msg.sender == owner);
         require(msg.messanger == sender);  //dummy text
