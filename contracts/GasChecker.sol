b336bf7c4848d889b21da9342aec0368717a683a
srihari-j <94820116+srihari-j@users.noreply.github.com>
2023-04-26 23:38:23 +0530

    test

--- contracts/GasChecker.sol
+++ contracts/GasChecker.sol
@@ -2,17 +2,23 @@
 
 pragma solidity ^0.8.19;
 
-contract GasChecker {
-    error GasCostDiffers(uint256 expected, uint256 actual);
+contract flipper {
+        bool private value;
 
-    modifier checkGasCost(uint256 expected) {
-        uint256 gas = gasleft();
-        _;
-        unchecked {
-            gas -= gasleft();
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        constructor(bool initvalue) {
+                value = initvalue;
         }
-        if (expected > 0 && gas != expected) {
-            revert GasCostDiffers(expected, gas);
+
+        /// A message that can be called on instantiated contracts.
+        /// This one flips the value of the stored `bool` from `true`
+        /// to `false` and vice versa.
+        function flip() public {
+                value = !value;
         }
-    }
+
+        /// Simply returns the current value of our `bool`.
+        function get() public view returns (bool) {
+                return value;
+        }
 }
