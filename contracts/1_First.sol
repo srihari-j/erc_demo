ad813e5176872b53b6f94f43189b60dc81221d29
srihari-j <94820116+srihari-j@users.noreply.github.com>
2023-05-01 16:29:10 +0530

    check

--- contracts/1_First.sol
+++ contracts/1_First.sol
@@ -1,17 +1,45 @@
-// SPDX-License-Identifier: MIT    
-pragma solidity ^0.8.19;    
-contract Sample {
-    uint public a = 0;
-    
-    function Sample() public {
-        a = 10;
-    }
-}
+pragma solidity >=0.4.25 <0.6.0;
 
-contract Sample2 {
-    uint public a = 0;
-    
-    function Sample2() public {
-        a = 10;
+contract HelloBlockchain
+{
+     //Set of States
+    enum StateType { Request, Respond}
+
+    //List of properties
+    StateType public  State;
+    address public  Requestor;
+    address public  Responder;
+
+    string public RequestMessage;
+    string public ResponseMessage;
+
+    // constructor function
+    constructor(string memory message) public
+    {
+        Requestor = msg.sender;
+        RequestMessage = message;
+        State = StateType.Request;
+    }
+
+    // call this function to send a request
+    function SendRequest(string memory requestMessage) public
+    {
+        if (Requestor != msg.sender)
+        {
+            revert();
+        }
+
+        RequestMessage = requestMessage;
+        State = StateType.Request;
+    }
+
+    // call this function to send a response
+    function SendResponse(string memory responseMessage) public
+    {
+        Responder = msg.sender;
+
+        // call ContractUpdated() to record this action
+        ResponseMessage = responseMessage;
+        State = StateType.Respond;
     }
 }
