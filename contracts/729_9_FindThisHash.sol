0056d1a186e7567fa95bfcfa63290819565fef58
srihari-j <94820116+srihari-j@users.noreply.github.com>
2024-09-13 13:03:33 +0530

    Update 729_9_FindThisHash.sol

--- contracts/729_9_FindThisHash.sol
+++ contracts/729_9_FindThisHash.sol
@@ -4,7 +4,8 @@
  * @vulnerable_at_lines: 17
  */
 
-pragma solidity ^0.4.22;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 contract FindThisHash {
     bytes32 constant public hash = 0xb5b5b97fafd9855eec9b41f74dfb6c38f5951141f9a3ecd7f44d5479b630ee0a;
